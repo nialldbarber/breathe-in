06170f8cb28477ac8caf4ae00ab19745e4e2a93e
Niall Barber <niall.barber@actual-experience.com>
2021-10-05 09:26:23 +0100

    Added badge animation

--- README.md
+++ README.md
@@ -1,17 +1,18 @@
 Todos:
 
+- [ ] badge select animation
+- [ ] countdown before exercise starts
 - [ ] hold countdown animation
-- [ ] breathing page redesign
+- [ ] lottie animations
+- [ ] get first name from localstorage?
+- [ ] info popups
+- [ ] custom breathing exercises
+- [ ] dark mode
 - [ ] formalise imports
 - [ ] make prettier & eslint better
-- [ ] lottie animations
-- [ ] badge select animation
-- [ ] get first name from localstorage?
-- [ ] instructions
-- [ ] countdown before exercise starts
-- [ ] dark mode
-- [ ] custom breathing exercises
-- [ ] info popups
+- [ ] tests
+- [x] instructions
+- [x] breathing page redesign
 - [x] time of day greeting
 - [x] buzz for exercise intervals
 - [x] breathing times, EG 4, 4, 4, 4
--- components/Badges/index.tsx
+++ components/Badges/index.tsx
@@ -1,69 +1,14 @@
 import React from 'react';
-import {
-  StyleSheet,
-  ScrollView,
-  Text,
-  View,
-  TouchableOpacity,
-} from 'react-native';
-import {useTheme} from '@react-navigation/native';
-import {
-  widthPercentageToDP as wp,
-  heightPercentageToDP as hp,
-} from 'react-native-responsive-screen';
+import {ScrollView} from 'react-native';
+import BadgeInner from './BadgeInner';
 import {feelings} from '../../constants/exercises';
-import {useDispatch} from 'react-redux';
-import {filterBySelectedBadge} from '../../store/slices/exercises';
 
 export default function Badges() {
-  const {colors} = useTheme();
-
-  const styles = StyleSheet.create({
-    badgeContainer: {
-      marginTop: hp('2%'),
-      marginBottom: hp('4%'),
-    },
-    badge: {
-      backgroundColor: colors.primaryFaded,
-      paddingHorizontal: 12,
-      paddingVertical: 6,
-      borderRadius: 20,
-      marginRight: 10,
-      borderColor: colors.primary,
-      borderWidth: 1,
-    },
-    badgeText: {
-      color: colors.primary,
-      fontWeight: '800',
-      fontSize: wp('4%'),
-    },
-  });
-
-  const dispatch = useDispatch();
-
-  // TODO: click event
-
   return (
-    <View style={styles.badgeContainer}>
-      <ScrollView horizontal showsHorizontalScrollIndicator={false}>
-        {feelings.map((item, i) => (
-          <TouchableOpacity
-            key={i}
-            activeOpacity={1}
-            onPress={() => {
-              dispatch(filterBySelectedBadge(feelings[i]));
-            }}
-            style={[
-              styles.badge,
-              {
-                marginLeft: i === 0 ? wp('5%') : 0,
-              },
-            ]}
-          >
-            <Text style={styles.badgeText}>{item}</Text>
-          </TouchableOpacity>
-        ))}
-      </ScrollView>
-    </View>
+    <ScrollView horizontal showsHorizontalScrollIndicator={false}>
+      {feelings.map((item, i) => (
+        <BadgeInner {...{i, item}} />
+      ))}
+    </ScrollView>
   );
 }
